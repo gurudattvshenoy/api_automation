735885fcf60e665b3bf6303e430ed9398d46ae6a
gurudattvshenoy <guruvshenoy@gmail.com>
2020-09-05 08:01:18 +0530

    changing test case naming convention

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,7 +3,7 @@ from libs.common.Assertion import Assertion
 from libs.userapi.UserApi import UserApi
 
 
-def Verify_user_can_be_created_rest_post_api_call():
+def TestCase_01_verify_user_can_be_created_rest_post_api_call():
 
     response = UserApi().create_user("Gurudatt", "SDET Engineer")
     response_status_code = response.get("status_code")
@@ -15,7 +15,7 @@ def Verify_user_can_be_created_rest_post_api_call():
     Assertion.assert_equal(response_body.get("job") ,"SDET Engineer",
                             "Failed: Job information in the response does not match with expected value")
 
-def Verify_user_can_be_updated_rest_put_api_call():
+def TestCase_02_Verify_user_can_be_updated_rest_put_api_call():
 
     response = UserApi().update_user(2,"guru","SDET")
     response_status_code = response.get("status_code")
@@ -23,7 +23,7 @@ def Verify_user_can_be_updated_rest_put_api_call():
     Assertion.assert_equal(response_status_code, HTTPStatus.OK,
                            "Failed: API failed to respond with correct status code")
 
-def Verify_user_can_be_fetched_using_rest_get_api_call():
+def TestCase_03_Verify_user_can_be_fetched_using_rest_get_api_call():
 
     response = UserApi().get_user_by_id(2)
     response_status_code = response.get("status_code")
@@ -39,7 +39,7 @@ def Verify_user_can_be_fetched_using_rest_get_api_call():
                            "Failed: Expected last name does not match the actual")
 
 
-def Verify_user_can_be_deleted_using_rest_delete_api_call():
+def TestCase_04_Verify_user_can_be_deleted_using_rest_delete_api_call():
 
     response = UserApi().delete_user(2)
     response_status_code = response.get("status_code")
